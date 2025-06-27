d51f8a260f53855dbeb944c0bf34f8f311988047
nakuwa23 <b.nakuwa@alustudent.com>
2025-06-27 03:35:27 -0700

    Script 2

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+last_digit = number % 10  # keeps sign consistent for negative numbers in Python
 
 print(f"Last digit of {number} is {last_digit}", end='')
 
