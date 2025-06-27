05b56f231e9d548f3143d79ae4ad700984b2e870
nakuwa23 <b.nakuwa@alustudent.com>
2025-06-27 02:59:13 -0700

    Script 9

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(int(str.find("Python") > -1))
+print(int(str[0] == 'P'))
