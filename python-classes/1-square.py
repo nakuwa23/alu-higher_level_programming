560124a86a2c33d09a9c98284edb667b68681457
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-09 04:33:41 -0700

    Script 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a square class that does nothing"""
+
+
+class Square:
+    """This is a square class that stores a size"""
+
+    def __init__(self, size):
+        """This function is the instantiator"""
+
+        self._Square__size = size
