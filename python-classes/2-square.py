cea65a9a22279929deae078682f67db563dfb112
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-09 04:40:35 -0700

    Script 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
 """This is a square class that does nothing"""
+
+
 class Square:
-   
     """This is a square class that checks if the initial value is an integer"""
+
     def __init__(self, size=0):
-       
         """This function checks if the argument is an integer"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
