523c8b5d985e646be74ac63783e6db6ba2d01339
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-11 06:51:04 -0700

    Script 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+"""
+This module show the inheritance process in python
+"""
+
+
 class MyList(list):
+    """This class is inherited from the list class"""
+
     def print_sorted(self):
+        """This function print the sorted list input"""
+        
         print(sorted(self))
