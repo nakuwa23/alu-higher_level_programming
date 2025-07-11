20b427ff3fc6fa0d9d4edd593bcbb11a8bce4434
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-11 07:29:27 -0700

    Script 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module has an empty class"""
+
+
+class BaseGeometry:
+    """This is a class that does practically nothing"""
+
+    def area(self):
+        """This function raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This function check if the value is valid"""
+        
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+# idk tbh
