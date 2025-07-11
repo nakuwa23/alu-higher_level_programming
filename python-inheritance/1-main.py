1fb1041018739e3d5d522821a085ff2a57ae9d86
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-11 06:46:56 -0700

    Script 1

---
+++ python-inheritance/1-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+
+print(my_list)        # Should print: [1, 4, 2, 3, 5]
+my_list.print_sorted()  # Should print: [1, 2, 3, 4, 5]
+print(my_list)        # Should print: [1, 4, 2, 3, 5]
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,3 @@
+class MyList(list):
+    def print_sorted(self):
+        print(sorted(self))
