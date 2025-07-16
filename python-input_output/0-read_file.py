ecdf3663b665ffb99dcd515b385b6a9bcd171e2d
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-16 10:04:37 -0700

    Script 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,9 +3,10 @@
 This module reads a text file
 """
 
+
 def read_file(filename=""):
     """
-    This function reads a text file
+    This function reads a text file and prints its content to stdout.
     """
-    with open(filename, 'r'):
-        pass
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
