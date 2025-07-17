e9495816ec78668238f6071bc52d0b5f0fc1122a
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-17 03:37:42 -0700

    Script 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a class"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This method returns a json
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
+
+    def reload_from_json(self, json):
+        """I don't really know about this one"""
+        for atr in json:
+            self.__dict__[atr] = json[atr]
