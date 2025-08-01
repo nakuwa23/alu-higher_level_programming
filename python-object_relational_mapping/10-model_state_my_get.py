690507382bc14e322708baafdca3f37a4f0d52b9
nakuwa23 <b.nakuwa@alustudent.com>
2025-08-01 04:08:21 -0700

    Script 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database."""
+"""
+Script that prints the State object with the name passed as argument
+from the database
+Using module SQLAlchemy
+"""
 
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
+from sys import argv
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username, password, db_name),
-        pool_pre_ping=True
-    )
-
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
+    Base.metadata.create_all(engine)
 
-    state = session.query(State).filter(State.name == state_name).first()
+    s_tate = session.query(State).filter(State.name == argv[4]).first()
 
-    if state:
-        print(state.id)
+    if s_tate:
+        print("{}".format(s_tate.id))
     else:
         print("Not found")
-
     session.close()
-
