0f0d73c354e7ef123278d49517cdf18ec7e30638
nakuwa23 <b.nakuwa@alustudent.com>
2025-08-01 03:39:14 -0700

    Script 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -8,23 +8,22 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Get credentials and search name from command-line arguments
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Create engine for database connection
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{db_name}',
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name),
         pool_pre_ping=True
     )
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Search for the state by name (safe from SQL injection)
     state = session.query(State).filter(State.name == state_name).first()
 
-    # Print result
     if state:
         print(state.id)
     else:
